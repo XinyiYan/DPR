59303ac7b8a02d380ec8e94e16558976be1cb755
Ahmed Salamah <ahamsala@cdr352.int.cedar.computecanada.ca>
2020-11-17 01:27:51 -0800

    new

--- pytourch_download.sh
+++ pytourch_download.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-#SBATCH --cpus-per-task=6  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
+#SBATCH --cpus-per-task=1  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=32G       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
 #SBATCH --time=0-03:00
 #SBATCH --output=%N-%j.out
 #SBATCH --gres=gpu:p100:1
 
-module load python/3.6
-virtualenv --no-download $SLURM_TMPDIR/env
-source $SLURM_TMPDIR/env/bin/activate
-module load python/3.6
-pip install torch --no-index
-pip install --no-index torch torchvision torchtext torchaudio
-pip install --no-index transformers
+module load python/3.6.3
+source /home/ahamsala/torch_DPR/bin/activate
+# virtualenv --no-download $SLURM_TMPDIR/env
+# source $SLURM_TMPDIR/env/bin/activate
+# module load python/3.6
+# pip install torch --no-index
+# pip install --no-index torch torchvision torchtext torchaudio
+# pip install --no-index transformers
 
 # module load nixpkgs/16.09  gcc/7.3.0
 # module load faiss/1.6.2
@@ -23,7 +24,7 @@ pip install --no-index transformers
 # python data/download_data.py --resource data.retriever.qas.nq
 
 python -m torch.distributed.launch \
-	--nproc_per_node=6 train_dense_encoder.py \
+	--nproc_per_node=1 train_dense_encoder.py \
 	--max_grad_norm 2.0 \
 	--encoder_model_type hf_bert \
 	--pretrained_model_cfg bert-base-uncased \
