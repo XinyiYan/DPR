1b2c4a8d5ef1009b5eb6fdff81182f986f02974b
Pierre McWhannel <pmcw@cedar5.cedar.computecanada.ca>
2020-12-02 19:09:30 -0800

    Files to setup env for creating embeddings.

--- ctx_1.sh
+++ ctx_1.sh
@@ -6,20 +6,9 @@
 #SBATCH --gres=gpu:p100:1
 
 
+# Change to where you make your virtual environment
 module load python/3.6.3
-virtualenv --no-download $SLURM_TMPDIR/torch_DPR
-source $SLURM_TMPDIR/torch_DPR/bin/activate
-module load python/3.6.3
-pip install torch --no-index
-pip install --no-index torch torchvision torchtext torchaudio
-pip install --no-index transformers==3.0.2
-pip install spacy[cuda] --no-index
-module load nixpkgs/16.09  gcc/7.3.0  cuda/10.1
-export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
-module load python/3.6.3
-module load faiss/1.6.2
-deactivate
-source $SLURM_TMPDIR/torch_DPR/bin/activate
+source virtual_DPR/bin/activate
 
 # mkdir data/checkpoint
 # wget https://dl.fbaipublicfiles.com/dpr/checkpoint/retriver/multiset/hf_bert_base.cp data/checkpoint
